bf95262fa391235b27bf3b1a88f93c0a412c337d
Phiwo Njemla <phiwo0415@gmail.com>
2023-11-29 15:23:48 -0800

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
 # creates a file in /tmp
 
 file { '/tmp/school':
- content => 'I love Puppet',
- mode    => '0744',
- owner   => 'www-data',
- group   => 'www-data',
+  ensure  => file,
+  content => 'I love Puppet',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
 }
