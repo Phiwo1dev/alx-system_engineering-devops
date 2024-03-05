98bbebb671693b47b30f90a27c4a8df9c747b20d
Phiwo <phiwo041590@gmail.com>
2024-03-05 20:11:55 +0000

    task 1 and 2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
 """
-This function queries the Reddit API and returns total number of
-subscribers for a given subreddit.
+A function that queries subscribers on a given Reddit subreddit.
 """
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    A function that queries the Reddit API
-    - returns 0, If not a valid subreddit.
-    """
-    req = requests.get(
-        "https://www.reddit.com/r/{}/about.json".format(subreddit),
-        headers={"User-Agent": "Custom"},
-    )
-
-    if req.status_code == 200:
-        return req.json().get("data").get("subscribers")
+    """Returns total number of subscribers on a given subreddit"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
     else:
         return 0
